da9fbec55c3eb9621addb771e947ac303298a38c
smemane <shwetasmemane@gmail.com>
2018-06-13 01:07:38 -0700

    add 2 way databinding, remove onInput event, [(ngModel)]. Needs addind FormsModule to app.module.ts

--- Angular-complete-guide/src/app/servers/servers.component.ts
+++ Angular-complete-guide/src/app/servers/servers.component.ts
@@ -17,10 +17,6 @@ export class ServersComponent implements OnInit {
     }, 2000);
   }
 
-  onUserInput = (event: Event): void => {
-    this.serverName = (<HTMLInputElement>event.target).value;
-    console.log("Added a new server " + this.serverName);
-  }
   onServerCreated = (event: Event): void => {
     this.allowNewServer = false; 
     console.log("New server was created by target " + (<HTMLButtonElement>event.target));
